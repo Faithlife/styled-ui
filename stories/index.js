94ebc66bf35efc0bfc2d6bebf92208be9d2aefc2
Dustin Masters <dustin.masters@faithlife.com>
2018-04-09 14:33:48 -0700

    Improve button style

--- stories/index.js
+++ stories/index.js
@@ -6,7 +6,7 @@ import { action } from '@storybook/addon-actions';
 import Button from '../components/demo-button/container.jsx';
 
 storiesOf('Button', module)
-	.add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+	.add('with text', () => <Button onClick={action('clicked')}>Click here to change the button style</Button>)
 	.add('with some emoji', () => (
 		<Button onClick={action('clicked')}>
 			<span role="img" aria-label="so cool">
