04a17cf096d168c2bf17f24fc5b5d70299a7fc76
Dustin Masters <dustin.masters@faithlife.com>
2018-04-09 23:13:11 -0700

    Improve readability of themeClassNames.
    The previous variable name was misleading.

--- components/utils/index.js
+++ components/utils/index.js
@@ -1,10 +1,10 @@
 import classNames from 'classnames';
 
-export function themeClassNames(baseTheme, overridenTheme, propertyNames) {
+export function themeClassNames(baseTheme, overridingTheme, propertyNames) {
 	return classNames(
 		propertyNames.reduce(
 			(accumulator, currentValue) =>
-				accumulator.concat([baseTheme[currentValue], (overridenTheme || {})[currentValue]]),
+				accumulator.concat([baseTheme[currentValue], (overridingTheme || {})[currentValue]]),
 			[],
 		),
 	);
