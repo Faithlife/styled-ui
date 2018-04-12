e84c260cacfeced78c5c6a9851660fa4cf2c6323
Dustin Masters <dustin.masters@faithlife.com>
2018-04-12 15:37:17 -0700

    Add markdown preview

--- stories/demo-button/ok-cancel.jsx
+++ stories/demo-button/ok-cancel.jsx
@@ -2,16 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '../../components';
 import styles from './story-styles.less';
+import docs from './ok-cancel.md';
 
 export default class DemoContainer extends Component {
 	static propTypes = {
-		children: PropTypes.node.isRequired,
 		onClick: PropTypes.func.isRequired,
 		theme: PropTypes.object,
-	};
-
-	state = {
-		clicked: false,
 	};
 
 	onClick = () => {
@@ -23,29 +19,30 @@ export default class DemoContainer extends Component {
 	};
 
 	render() {
-
-
 		return (
-			<div className={styles.demos}>
-				<div className={styles.demoRow}>
-					<Button
-						onClick={this.onClick}
-						theme={this.props.theme}
-						variations={['secondary', 'large']}
-					>
-						Cancel
-				</Button>
+			<div>
+				<div className={styles.demos}>
+					<div className={styles.demoRow}>
+						<Button
+							onClick={this.onClick}
+							theme={this.props.theme}
+							variations={['secondary', 'large']}
+						>
+							Cancel
+						</Button>
+					</div>
+					<div className={styles.demoRow}>
+						<Button
+							onClick={this.onClick}
+							theme={this.props.theme}
+							variations={['primary', 'large']}
+						>
+							Okay
+						</Button>
+					</div>
 				</div>
-				<div className={styles.demoRow}>
-					<Button
-						onClick={this.onClick}
-						theme={this.props.theme}
-						variations={['primary', 'large']}
-					>
-						Okay
-				</Button>
-				</div>
-			</div >
+				<div className={styles.documentation} dangerouslySetInnerHTML={{ __html: docs }} />
+			</div>
 		);
 	}
 }
