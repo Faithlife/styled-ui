323b59a9950d86f7839d5fc9b2900eee8eeb82b4
Dustin Masters <dustin.masters@faithlife.com>
2018-06-26 15:03:51 -0700

    Override label margin to 0.375rem (6px)

--- components/bootstrap/base-components.jsx
+++ components/bootstrap/base-components.jsx
@@ -1,12 +1,24 @@
 import * as _Bootstrap from 'reactstrap';
+import styled from 'styled-components';
 import { wrapBootstrap } from './util';
 
 const inlineComponents = ['Button'];
 
-export default Object.keys(_Bootstrap).reduce(
+const Components = Object.keys(_Bootstrap).reduce(
 	(prev, curr) => ({
 		...prev,
 		[curr]: wrapBootstrap(_Bootstrap[curr], inlineComponents.includes(curr)),
 	}),
 	{},
 );
+
+export default {
+	...Components,
+	Label: styled(_Bootstrap.Label)`
+		&& {
+			font-size: 14px;
+			line-height: 1em;
+			margin-bottom: 0.375rem;
+		}
+	`,
+};
