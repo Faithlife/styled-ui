749332c859d570530b2eb04d1a8a0c05537f937d
Dustin Masters <dustin.masters@faithlife.com>
2018-07-31 16:46:34 -0700

    Export async typeahead

--- components/text-input/typeahead.jsx
+++ components/text-input/typeahead.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Typeahead as _Typeahead } from 'react-bootstrap-typeahead';
+import {
+	AsyncTypeahead as _AsyncTypeahead,
+	Typeahead as _Typeahead,
+} from 'react-bootstrap-typeahead';
 import { BootstrapContainer } from '../utils';
 import './custom.scss';
+
+const StyledAsyncTypeahead = styled(_AsyncTypeahead)`
+	.rbt-highlight-text {
+		padding: 0;
+		font-weight: bold;
+		background-color: inherit;
+	}
+
+	&&& {
+		.rbt-token-remove-button {
+			top: 0;
+		}
+	}
+`;
 
 const StyledTypeahead = styled(_Typeahead)`
 	.rbt-highlight-text {
@@ -17,6 +34,12 @@ const StyledTypeahead = styled(_Typeahead)`
 		}
 	}
 `;
+
+export const AsyncTypeahead = props => (
+	<BootstrapContainer>
+		<StyledAsyncTypeahead {...props} />
+	</BootstrapContainer>
+);
 
 export const Typeahead = props => (
 	<BootstrapContainer>
