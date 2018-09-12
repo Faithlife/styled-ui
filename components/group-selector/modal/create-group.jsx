e71c1c7d1240f938e8a2b32c4c02af9c192deab6
Jordan Sjodin <jordan.sjodin@logos.com>
2018-09-12 10:17:34 -0700

    Move create button up one level
    Focus church field on mount

--- components/group-selector/modal/create-group.jsx
+++ components/group-selector/modal/create-group.jsx
@@ -3,50 +3,40 @@ import PropTypes from 'prop-types';
 import { Bootstrap } from '../../../components/main.js';
 import * as Styled from '../styled.jsx';
 
-const { Button, Input } = Bootstrap;
+const { Input } = Bootstrap;
 
 export class CreateGroup extends React.Component {
 	static propTypes = {
-		onCreateGroup: PropTypes.func.isRequired,
 		onChurchNameInputChange: PropTypes.func.isRequired,
 		newChurchName: PropTypes.string,
 		onChurchLocationInputChange: PropTypes.func.isRequired,
 		newChurchLocation: PropTypes.string,
-		showButton: PropTypes.bool.isRequired,
 	};
+
+	churchNameInput = React.createRef();
+
+	componentDidMount() {
+		this.churchNameInput.current.focus();
+	}
 
 	render() {
 		return (
 			<Styled.CreateGroup>
 				<Styled.CreateGroupLabel>Church Name</Styled.CreateGroupLabel>
 				<Input
-					style={{ marginBottom: '20px', marginTop: '4px' }}
+					innerRef={this.churchNameInput}
+					value={this.props.newChurchName}
 					onChange={this.props.onChurchNameInputChange}
 					placeholder="Church name"
 					bsSize="lg"
 				/>
-				<div>
-					<Styled.CreateGroupLabel>Church Location</Styled.CreateGroupLabel>
-					<Input
-						style={{ marginTop: '4px' }}
-						value={this.props.newChurchLocation}
-						placeholder="City, State"
-						onChange={this.props.onChurchLocationInputChange}
-						bsSize="lg"
-					/>
-					{this.props.showButton && (
-						<Styled.CreateGroupButtonWrapper>
-							<Styled.CreateGroupButtonText>Don't see your church?</Styled.CreateGroupButtonText>
-							<Button
-								color="primary"
-								disabled={this.props.newChurchName === '' || this.props.newChurchLocation === ''}
-								onClick={this.props.onCreateGroup}
-							>
-								Create
-							</Button>
-						</Styled.CreateGroupButtonWrapper>
-					)}
-				</div>
+				<Styled.CreateGroupLabel>Church Location</Styled.CreateGroupLabel>
+				<Input
+					value={this.props.newChurchLocation}
+					placeholder="City, State"
+					onChange={this.props.onChurchLocationInputChange}
+					bsSize="lg"
+				/>
 			</Styled.CreateGroup>
 		);
 	}
