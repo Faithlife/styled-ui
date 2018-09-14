a0eafc8df959baa3e18ff14009802429c1d771ef
ToddWhite <todd.white@logos.com>
2018-09-13 17:04:24 -0700

    Add support for forwarding ref to the actual DOM element

--- components/button/component.jsx
+++ components/button/component.jsx
@@ -1,21 +1,25 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { forwardClassRef } from '../utils/forwardref-wrapper.jsx';
 import * as Styled from './styled.jsx';
 import { BaseButton } from './base-button.jsx';
 
 /** Standard button with transition styles */
-export class Button extends PureComponent {
-	static propTypes = {
-		...BaseButton.propTypes,
-		/** The type of button (for instance, submit) */
-		type: PropTypes.string,
-	};
+export const Button = forwardClassRef(
+	class Button extends PureComponent {
+		static propTypes = {
+			...BaseButton.propTypes,
+			/** The type of button (for instance, submit) */
+			type: PropTypes.string,
+		};
 
-	static defaultProps = {
-		type: 'button',
-	};
+		static defaultProps = {
+			type: 'button',
+		};
 
-	render() {
-		return <BaseButton baseComponent={Styled.Button} {...this.props} />;
-	}
-}
+		render() {
+			const { forwardedRef, ...props } = this.props;
+			return <BaseButton baseComponent={Styled.Button} {...props} ref={forwardedRef} />;
+		}
+	},
+);
