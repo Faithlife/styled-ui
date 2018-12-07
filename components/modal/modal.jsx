Faithlife/styled-ui

commit e2a3db25011e82fb85fbd44798f1f9aaa586b37c

Use portal to render modals outside of any stacking contexts (#76)
https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Positioning/Understanding_z_index/The_stacking_context

components/modal/modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { debouncedResize } from '../utils';
 import { ModalBackdrop } from '../modal-backdrop/component.jsx';
@@ -53,11 +54,13 @@ export class Modal extends React.Component {
 
 	state = {
 		modalWidth: null,
+		canUseDom: false,
 	};
 
 	componentDidMount() {
 		const { cancel } = debouncedResize(this.handleResize);
 		this.cancelResizeListener = cancel;
+		this.setState({ canUseDom: true }); // eslint-disable-line
 	}
 
 	componentWillUnmount() {
@@ -70,22 +73,17 @@ export class Modal extends React.Component {
 		}
 	};
 
-	render() {
+	renderModal() {
 		const {
 			theme,
 			title,
 			subtitle,
 			onClose,
-			isOpen,
 			children,
 			renderFooter,
 			footerProps,
 			withoutFooter,
 		} = this.props;
-
-		if (!isOpen) {
-			return null;
-		}
 
 		const { modalWidth } = this.state;
 
@@ -116,5 +114,13 @@ export class Modal extends React.Component {
 				</ModalBackdrop>
 			</ThemeProvider>
 		);
+	}
+
+	render() {
+		if (!this.props.isOpen || !this.state.canUseDom) {
+			return null;
+		}
+
+		return createPortal(this.renderModal(), document.body);
 	}
 }

commit 57016951540ccc6a39db08eba4a101312c9d6d39

Add 'container' prop to modal
This reverts a breaking change introduced in #76

components/modal/modal.jsx
@@ -48,6 +48,8 @@ export class Modal extends React.Component {
 		renderFooter: PropTypes.func,
 		/** No footer will be rendered if withoutFooter is true */
 		withoutFooter: PropTypes.bool,
+		/** Set to 'body' to attach the modal to body, otherwise will attach as a child element */
+		container: PropTypes.string,
 	};
 
 	static defaultProps = {
@@ -66,6 +68,18 @@ export class Modal extends React.Component {
 		const { cancel } = debouncedResize(this.handleResize);
 		this.cancelResizeListener = cancel;
 		this.setState({ canUseDom: true }); // eslint-disable-line
+
+		const { container } = this.props;
+		if (container) {
+			if (typeof container === 'string') {
+				// must be an id or body
+				this.targetContainer =
+					container === 'body' ? document.body : document.getElementById(container);
+			} else {
+				// must be a ref
+				this.targetContainer = typeof container === 'object' ? container.current : container();
+			}
+		}
 	}
 
 	componentWillUnmount() {
@@ -132,6 +146,10 @@ export class Modal extends React.Component {
 			return null;
 		}
 
-		return createPortal(this.renderModal(), document.body);
+		if (this.targetContainer != null) {
+			return createPortal(this.renderModal(), this.targetContainer);
+		}
+
+		return this.renderModal();
 	}
 }

components/share-dialog/component.jsx
@@ -34,7 +34,7 @@ export class ShareDialog extends React.Component {
 		const encodedMessage = message ? encodeURIComponent(message) : '';
 
 		return (
-			<Modal withoutFooter isOpen={isOpen} onClose={onClose} title={modalTitle}>
+			<Modal withoutFooter isOpen={isOpen} onClose={onClose} title={modalTitle} container="body">
 				<Styled.ShareContainer>
 					<FaithlifeShareButton encodedShareUrl={encodedShareUrl} encodedMessage={encodedMessage} />
 					<TwitterShareButton encodedShareUrl={encodedShareUrl} encodedMessage={encodedMessage} />

components/utils/index.js
@@ -75,4 +75,12 @@ export const resetStyles = `
 	*:after {
 		box-sizing: border-box;
 	}
+
+	&::placeholder {
+		font-style: initial;
+	}
+
+	p {
+		padding: 0;
+	}
 `;