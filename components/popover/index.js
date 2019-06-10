c506afed466424f626bae515e660ad1c03859b7e
Scott Maxa <scott.maxa@faithlife.com>
2019-06-10 09:23:17 -0700

    Add unstyled popover.

--- components/popover/index.js
+++ components/popover/index.js
@@ -1,4 +1,5 @@
 export { Popover } from './component';
+export { UnstyledPopover } from './unstyled-popover';
 export { PopoverManager } from './popover-manager';
 export { PopoverReference } from './popper-helpers';
 export { Tooltip } from './tooltip';
