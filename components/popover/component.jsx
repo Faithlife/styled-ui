7c8d467d65b3a4906bd0a98103902844c1faaf7c
Scott Maxa <scott.maxa@faithlife.com>
2019-06-10 11:34:28 -0700

    Convert popover to a function component.

--- components/popover/component.jsx
+++ components/popover/component.jsx
@@ -1,43 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { colors } from '../shared-styles';
 import { UnstyledPopover } from './unstyled-popover';
 
 /** Pre-styled popover that uses unstyled popovers internally. */
-export class Popover extends Component {
-	static propTypes = {
-		...UnstyledPopover.propTypes,
-	};
-
-	static defaultProps = {
-		placement: 'top',
-		theme: {
-			backgroundColor: colors.white,
-		},
-		styleOverrides: {
-			borderRadius: '3px',
-			padding: '12px',
-			maxWidth: '300px',
-		},
-	};
-
-	render() {
-		const { children, placement, theme, styleOverrides, ...otherProps } = this.props;
-		const { ...otherStyles } = styleOverrides;
-		const newStyle = {
-			...otherStyles,
-			borderRadius: '3px',
-			padding: '12px',
-			maxWidth: '300px',
-		};
-		return (
-			<UnstyledPopover
-				{...otherProps}
-				placement={placement}
-				theme={theme}
-				styleOverrides={newStyle}
-			>
-				{children}
-			</UnstyledPopover>
-		);
-	}
+export function Popover(props) {
+	const { children, placement, theme, styleOverrides, ...otherProps } = props;
+	const newStyle = { ...Popover.defaultProps.styleOverrides, ...styleOverrides };
+	return (
+		<UnstyledPopover
+			{...otherProps}
+			placement={placement || 'top'}
+			theme={theme || { ...Popover.defaultProps.theme }}
+			styleOverrides={newStyle || {}}
+		>
+			{children}
+		</UnstyledPopover>
+	);
 }
+
+Popover.propTypes = { ...UnstyledPopover.propTypes };
+
+Popover.defaultProps = {
+	placement: 'top',
+	theme: { backgroundColor: colors.white },
+	styleOverrides: { borderRadius: '3px', padding: '12px', maxWidth: '300px' },
+};
