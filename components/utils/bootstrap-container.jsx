370643cfef09b26b4cbe8e0562078c87e1ea24d2
Scott Maxa <scott.maxa@faithlife.com>
2019-06-11 16:50:09 -0700

    Display warning on Bootstrap elements.

--- components/utils/bootstrap-container.jsx
+++ components/utils/bootstrap-container.jsx
@@ -3,23 +3,39 @@ import PropTypes from 'prop-types';
 
 const { Provider, Consumer } = React.createContext({});
 
-export const BootstrapContainer = ({ children, styles }) => (
-	<Consumer>
-		{value =>
-			value.inCssResetScope ? (
-				children
-			) : (
-				<Provider value={{ inCssResetScope: true }}>
-					<BootstrapContainer styles={styles}>
-						<div className="fl-b" style={styles}>
-							{children}
-						</div>
-					</BootstrapContainer>
-				</Provider>
-			)
+export class BootstrapContainer extends React.Component {
+	static propTypes = { children: PropTypes.node.isRequired, styles: PropTypes.object };
+
+	componentDidMount() {
+		if (process.env.NODE_ENV !== 'production') {
+			console.warn(
+				'Warning: You are using a deprecated Bootstrap element. \n',
+				'Styled-UI has replaced all Bootstrap elements: https://faithlife.github.io/styled-ui/#/ \n',
+			);
 		}
-	</Consumer>
-);
+	}
+
+	render() {
+		const { children, styles } = this.props;
+		return (
+			<Consumer>
+				{value =>
+					value.inCssResetScope ? (
+						children
+					) : (
+						<Provider value={{ inCssResetScope: true }}>
+							<BootstrapContainer styles={styles}>
+								<div className="fl-b" style={styles}>
+									{children}
+								</div>
+							</BootstrapContainer>
+						</Provider>
+					)
+				}
+			</Consumer>
+		);
+	}
+}
 
 export function wrapBootstrap(Component, inline) {
 	const styles = inline ? { display: 'inline-block' } : {};
@@ -29,8 +45,3 @@ export function wrapBootstrap(Component, inline) {
 		</BootstrapContainer>
 	);
 }
-
-BootstrapContainer.propTypes = {
-	children: PropTypes.node.isRequired,
-	styles: PropTypes.object,
-};
