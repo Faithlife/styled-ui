f5c954aaf67f6371a7269984b8ef79878e7ecf45
Scott Maxa <scottkmaxa@gmail.com>
2019-06-25 17:09:37 -0700

    Refactor anchor button to a functional component.

--- components/button/anchor-button.jsx
+++ components/button/anchor-button.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { filterChildProps, forwardClassRef } from '../utils';
+import { filterChildProps } from '../utils';
 import { BaseButton } from './base-button';
 import { baseButtonProps } from './button-utils';
 
 /** Standard button with transition styles */
-export const AnchorButton = forwardClassRef(
-	class AnchorButton extends React.Component {
-		static propTypes = {
-			...BaseButton.propTypes,
-			/** The destination the AnchorButton should link to. */
-			href: PropTypes.string,
-			/** This should only be used as a last resort if Theme and StyleOverrides will not do what you need */
-			className: PropTypes.string,
-		};
+export function AnchorButton(props) {
+	const [{ disabled, ...buttonProps }, baseProps] = filterChildProps(props, baseButtonProps);
+	return <BaseButton disabled={disabled} as="a" {...baseProps} {...buttonProps} />;
+}
 
-		render() {
-			const [{ forwardedRef, ...buttonProps }, baseProps] = filterChildProps(
-				this.props,
-				baseButtonProps,
-			);
-			return <BaseButton ref={forwardedRef} as="a" {...baseProps} {...buttonProps} />;
-		}
-	},
-);
+AnchorButton.propTypes = {
+	...BaseButton.propTypes,
+	/** The destination the AnchorButton should link to. */
+	href: PropTypes.string,
+	/** This should only be used as a last resort if Theme and StyleOverrides will not do what you need */
+	className: PropTypes.string,
+};
