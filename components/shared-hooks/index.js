a4e7f52ef72fbe0b95847a5e31c118c1d4dd6c06
Scott Maxa <scottkmaxa@gmail.com>
2019-07-10 15:27:35 -0700

    Hide Tooltips on mobile (#186)

--- components/shared-hooks/index.js
+++ components/shared-hooks/index.js
@@ -1,3 +1,4 @@
 export { useId } from './use-id';
 export { useBasicMap } from './use-basic-map';
 export { useFocusAwayHandler, useAddInboundsElement } from './use-focus-away-handler';
+export { useDebouncedCallback } from './use-debounced-callback';
