c0e1b33998083013dbde66ae73c1df72b9b29934
Bryan Smith <bryan.smith@logos.com>
2019-07-17 10:41:16 -0700

    Add Paragraph component for block-level text content.

---
+++ components/Paragraph.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { textStyle, layout, border } from 'styled-system';
+import { themeGet } from '@styled-system/theme-get';
+import { typography, common } from '../theme/system';
+import { theme } from '../theme';
+
+export const Paragraph = styled.p`
+	display: block;
+	${themeGet('textStyles.c.16')};
+
+	${textStyle};
+	${common};
+	${layout};
+	${typography};
+	${border};
+`;
+
+Paragraph.defaultProps.theme = theme;
