bca824e115fdafbae2bff52d496783cdee3643ae
Scott Maxa <scottkmaxa@gmail.com>
2019-08-06 15:09:07 -0700

    Remove commented code.

--- components/tabs/styled.jsx
+++ components/tabs/styled.jsx
@@ -7,38 +7,6 @@ import { resetStyles } from '../utils';
 import { Text } from '../Text';
 const borderRadius = '3px 3px 0 0';
 
-// const selectedTab = css`
-// 	font-weight: 600;
-
-// 	&::before {
-// 		content: '';
-// 		position: absolute;
-// 		top: 0;
-// 		left: 0;
-// 		width: 100%;
-// 		height: 3px;
-// 		background-color: ${({ theme }) => theme.tabHighlightColor || colors.blueBase};
-
-// 		border-right: 1px solid ${({ theme }) => theme.tabHighlightColor || colors.blueBase};
-// 		border-left: 1px solid ${({ theme }) => theme.tabHighlightColor || colors.blueBase};
-// 		border-radius: ${borderRadius};
-// 	}
-
-// 	&::after {
-// 		content: '';
-// 		position: absolute;
-// 		top: 100%;
-// 		left: 1px;
-// 		width: calc(100% - 2px);
-// 		height: 1px;
-// 		background-color: ${({ theme }) => theme.activeBackgroundColor || 'white'};
-// 	}
-
-// 	background-color: ${({ theme }) => theme.activeBackgroundColor || 'white'};
-// 	border-right: 1px solid ${colors.gray14};
-// 	border-left: 1px solid ${colors.gray14};
-// `;
-
 export const Tab = styled.button.attrs({
 	role: 'tab',
 	'aria-selected': ({ selected }) => selected,
@@ -65,26 +33,6 @@ export const Tab = styled.button.attrs({
 		outline: none;
 	}
 `;
-
-// export const TabContent = styled.span.attrs({ tabIndex: -1 })`
-// 	border-radius: ${borderRadius};
-// 	cursor: pointer;
-// 	white-space: nowrap;
-// 	min-height: fit-content;
-// 	display: inline-block;
-// 	font-size: ${({ styleOverrides }) => styleOverrides.fontSize || thickness.sixteen};
-// 	width: ${({ styleOverrides }) => styleOverrides.width};
-// 	padding: ${({ styleOverrides }) =>
-// 		styleOverrides.padding || `${thickness.eight} ${thickness.sixteen}`};
-// 	background-color: ${({ theme }) => theme.inactiveBackgroundColor || colors.gray4};
-
-// 	&:focus {
-// 		outline: none;
-// 	}
-
-// 	${({ disabled }) => (disabled ? `color: ${colors.gray52}` : '')};
-// 	${({ selected }) => selected && selectedTab};
-// `;
 
 export const TabContent = styled(Text)`
 	&:focus {
@@ -126,28 +74,3 @@ const selectedTab = css`
 		height: 1px;
 	}
 `;
-
-// export const TabList = styled.div.attrs({ role: 'tablist' })`
-// 	border-bottom: 1px solid ${colors.gray14};
-// 	display: flex;
-// 	flex-direction: row;
-
-// 	& > *:not(:last-child) {
-// 		margin-right: ${thickness.eight};
-// 	}
-// `;
-
-// export const TabPanel = styled.div.attrs({
-// 	role: 'tabpanel',
-// 	id: ({ panelId }) => `panel:${panelId}`,
-// 	'aria-expanded': ({ selected }) => selected,
-// })`
-// 	position: relative;
-// 	padding: ${thickness.eight};
-
-// 	&:focus {
-// 		outline: none;
-// 	}
-
-// 	${({ selected }) => !selected && 'display: none'};
-// `;
