6423ab69ae8d7a73631791d0d7f946cc787bb71a
Scott Maxa <scottkmaxa@gmail.com>
2019-08-07 16:47:03 -0700

    Fix falsy issues in Popover.

--- components/popover/component.jsx
+++ components/popover/component.jsx
@@ -22,13 +22,45 @@ export function Popover(props) {
 			{...otherProps}
 			placement={placement}
 			borderRadius={borderRadius || 1}
-			padding={padding || 4}
-			paddingY={paddingY || padding || ''}
-			paddingX={paddingX || padding || ''}
-			paddingTop={paddingTop || paddingY || padding || ''}
-			paddingRight={paddingRight || paddingX || padding || ''}
-			paddingBottom={paddingBottom || paddingY || padding || ''}
-			paddingLeft={paddingLeft || paddingX || padding || ''}
+			padding={isDefined(padding) ? padding : 4}
+			paddingY={isDefined(paddingY) ? paddingY : isDefined(padding) ? padding : ''}
+			paddingX={isDefined(paddingX) ? paddingX : isDefined(padding) ? padding : ''}
+			paddingTop={
+				isDefined(paddingTop)
+					? paddingTop
+					: isDefined(paddingY)
+					? paddingY
+					: isDefined(padding)
+					? padding
+					: ''
+			}
+			paddingRight={
+				isDefined(paddingRight)
+					? paddingRight
+					: isDefined(paddingX)
+					? paddingX
+					: isDefined(padding)
+					? padding
+					: ''
+			}
+			paddingBottom={
+				isDefined(paddingBottom)
+					? paddingBottom
+					: isDefined(paddingY)
+					? paddingY
+					: isDefined(padding)
+					? padding
+					: ''
+			}
+			paddingLeft={
+				isDefined(paddingLeft)
+					? paddingLeft
+					: isDefined(paddingX)
+					? paddingX
+					: isDefined(padding)
+					? padding
+					: ''
+			}
 			maxWidth={maxWidth || '300px'}
 		>
 			{children}
@@ -40,4 +72,8 @@ Popover.propTypes = { ...PopoverBase.propTypes };
 
 Popover.defaultProps = {
 	placement: 'top',
+};
+
+const isDefined = function(value) {
+	return value !== undefined && value !== null;
 };
