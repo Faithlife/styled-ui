5bfd0ba8288e5354a7c0ad37ca58b533698b7855
Scott Maxa <scottkmaxa@gmail.com>
2019-08-14 11:12:02 -0700

    Remove theme prop from Radio.

--- components/radio/component.jsx
+++ components/radio/component.jsx
@@ -13,12 +13,6 @@ export class Radio extends Component {
 		onClick: PropTypes.func.isRequired,
 		title: PropTypes.string,
 		isChecked: PropTypes.bool,
-		theme: PropTypes.shape({
-			primary: PropTypes.string,
-			border: PropTypes.string,
-			disabledBackground: PropTypes.string,
-			disabledBorder: PropTypes.string,
-		}),
 		type: PropTypes.string,
 		children: PropTypes.node,
 		/** See the docs for how to override styles properly  */
