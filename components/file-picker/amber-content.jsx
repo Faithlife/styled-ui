3265839d31baf1c4415c527ab273fdd9753fb501
Virginia Pettit <veepower06@gmail.com>
2019-08-28 12:50:40 -0700

    smartly inject script

--- components/file-picker/amber-content.jsx
+++ components/file-picker/amber-content.jsx
@@ -1,10 +1,12 @@
-import React, { useRef, useCallback, useEffect } from 'react';
+import React, { useRef, useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useFilePickerContext } from './file-picker-helpers';
 import * as Styled from './styled';
 
 export function AmberContent({ accountId, filter, viewStyle }) {
 	const { allowMultiSelect, onCancel, onFilesSelected } = useFilePickerContext();
+
+	const amber = useAmber();
 
 	const handleMessage = useCallback(
 		event => {
@@ -33,8 +35,8 @@ export function AmberContent({ accountId, filter, viewStyle }) {
 	}, [handleMessage]);
 
 	useEffect(() => {
-		if (amberRef.current && window.amberfile) {
-			window.amberfile.embedded.load({
+		if (amberRef.current && amber) {
+			amber.embedded.load({
 				url: '/embed/',
 				container: amberRef.current,
 				groupId: accountId,
@@ -43,7 +45,7 @@ export function AmberContent({ accountId, filter, viewStyle }) {
 				filter,
 			});
 		}
-	}, [allowMultiSelect, filter, accountId, viewStyle]);
+	}, [allowMultiSelect, filter, accountId, viewStyle, amber]);
 
 	return <Styled.Tab ref={amberRef} />;
 }
@@ -56,3 +58,37 @@ AmberContent.propTypes = {
 	/** optional way to chose layout of the Amber embeded view */
 	viewStyle: PropTypes.string,
 };
+
+function useAmber() {
+	const [amber, setAmber] = useState(null);
+
+	useEffect(() => {
+		if (window.amberfile) {
+			setAmber(window.amberfile);
+		} else {
+
+				const onScriptLoad = () => {
+					setAmber(window.amberfile);
+				};
+
+				const onScriptError = (error) => {
+					console.error(error);
+				};
+
+				const amberScript = document.createElement('script');
+				amberScript.src = "https://amber.faithlife.com/scripts/api/embeddedBucket.js";
+				amberScript.async = true;
+				
+				amberScript.addEventListener('load', onScriptLoad);
+				amberScript.addEventListener('error', onScriptError);
+				
+				document.body.appendChild(amberScript);
+			}
+		return () => {
+			amberScript.removeEventListener('load', onScriptLoad);
+			amberScript.removeEventListener('error', onScriptError);
+		};
+	}, []);
+
+	return amber;
+}
