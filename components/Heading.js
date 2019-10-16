aa020e28164ae953cafd1b39fbf352717e801f45
Bryan Smith <bryanrsmith@gmail.com>
2019-10-16 15:13:54 -0700

    Fix typo.

--- components/Heading.js
+++ components/Heading.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Paragraph } from './Paragraph';
 
 export const Heading = React.forwardRef(({ level = 24, children, ...props }, ref) => (
-	<Paragraph as="heading" ref={ref} {...props} textStyle={`h.${level}`}>
+	<Paragraph as="header" ref={ref} {...props} textStyle={`h.${level}`}>
 		{children}
 	</Paragraph>
 ));
