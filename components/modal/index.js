4e7ca47ea354a032bfa1436b642c11d08b91add1
Robert Bolender <robert.bolender@faithlife.com>
2019-11-04 16:40:17 -0800

    (WIP) Add v6 Modal.
    Still a work in progress, this component is actually identical to the v5 LegacyModal.

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,7 +1,15 @@
 import { deprecate } from '../utils';
+import { Modal } from './modal';
+import { ModalHeader } from './modal-header';
 import { ModalContent } from './modal-content';
 import { ModalFooter } from './modal-footer';
+import { ModalFooterButtons } from './modal-footer-buttons';
 import { LegacyModal } from './legacy-modal';
+
+Modal.Header = ModalHeader;
+Modal.Content = ModalContent;
+Modal.Footer = ModalFooter;
+Modal.FooterButtons = ModalFooterButtons;
 
 const v6ImportHelpText = `You can opt into this new API by importing { modal } from @faithlife/styled-ui/v6
 Learn more at https://faithlife.github.io/styled-ui/#/modal/v6`;
@@ -17,6 +25,8 @@ const LegacyModalFooter = deprecate(
 );
 
 export {
+	// This is the new v6 modal
+	Modal,
 	// These legacy exports will be removed when v6 is released
 	LegacyModal,
 	LegacyModalContent,
---
+++ components/modal/modal.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
+import { Box } from '../Box';
+import { debouncedResize } from '../utils';
+import { ModalBackdrop } from '../modal-backdrop';
+import { ModalHeader } from './modal-header';
+import { DefaultModalFooter } from './default-modal-footer';
+import { ModalContent } from './modal-content';
+import { ModalSpacingContextProvider } from './use-modal-spacing';
+
+/**
+ * Modal with flexible contents. See also: SimpleModal
+ */
+export class Modal extends React.Component {
+	static propTypes = {
+		/** Controls state of modal */
+		isOpen: PropTypes.bool.isRequired,
+		/** Title of the modal */
+		title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+		/** Any explaining text to include in the modal header */
+		subtitle: PropTypes.string,
+		/** Callback function for when the modal is close  */
+		onClose: PropTypes.func.isRequired,
+		/** Contents of the modal */
+		children: PropTypes.node.isRequired,
+		/** Values for rendering an FL standard footer */
+		footerProps: PropTypes.shape({
+			commitButton: PropTypes.shape({
+				onClick: PropTypes.func.isRequired,
+				text: PropTypes.string.isRequired,
+				disabled: PropTypes.bool,
+			}),
+			cancelButton: PropTypes.shape({
+				onClick: PropTypes.func.isRequired,
+				text: PropTypes.string.isRequired,
+				disabled: PropTypes.bool,
+			}),
+			deleteButton: PropTypes.shape({
+				onClick: PropTypes.func.isRequired,
+				text: PropTypes.string.isRequired,
+				disabled: PropTypes.bool,
+			}),
+		}),
+		headerBottomBorder: PropTypes.string,
+		/** A default footer will be rendered if you don't supply a renderFooter function */
+		renderFooter: PropTypes.func,
+		/** No footer will be rendered if withoutFooter is true */
+		withoutFooter: PropTypes.bool,
+		/** Set to 'body' to attach the modal to body, otherwise will attach as a child element */
+		container: PropTypes.string,
+		/** 16px (default) or 24px */
+		variant: PropTypes.string,
+		/** h.16, h.18 (default), or h.24 */
+		headerVariant: PropTypes.string,
+		/** Intended for modals with lots of functionality, such as media galleries or editors */
+		fullscreen: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		variant: '16px',
+		headerVariant: 'h.18',
+	};
+
+	state = {
+		modalWidth: null,
+		canUseDom: false,
+	};
+
+	componentDidMount() {
+		const { cancel } = debouncedResize(this.handleResize);
+		this.cancelResizeListener = cancel;
+		this.setState({ canUseDom: true }); // eslint-disable-line
+
+		const { container } = this.props;
+		if (container) {
+			if (typeof container === 'string') {
+				// must be an id or body
+				this.targetContainer =
+					container === 'body' ? document.body : document.getElementById(container);
+			} else {
+				// must be a ref
+				this.targetContainer = typeof container === 'object' ? container.current : container();
+			}
+		}
+	}
+
+	componentWillUnmount() {
+		if (this.cancelResizeListener) {
+			this.cancelResizeListener();
+		}
+	}
+
+	handleResize = () => {
+		if (this._modal) {
+			this.setState({ modalWidth: this._modal.clientWidth });
+		}
+	};
+
+	renderModal() {
+		const {
+			title,
+			subtitle,
+			onClose,
+			children,
+			headerBottomBorder,
+			renderFooter,
+			footerProps,
+			withoutFooter,
+			theme,
+			styleOverrides,
+			variant,
+			headerVariant,
+			fullscreen,
+			...props
+		} = this.props;
+
+		const { modalWidth } = this.state;
+
+		const verticalButtons =
+			modalWidth &&
+			!renderFooter &&
+			(modalWidth < 220 ||
+				(modalWidth < 320 && footerProps && Object.keys(footerProps).length === 3));
+
+		const doesChildrenIncludeModalContent =
+			React.Children.count(children) === 1 && React.Children.only(children).type === ModalContent;
+
+		return (
+			<ModalBackdrop onClose={onClose} zIndex={(styleOverrides && styleOverrides.zIndex) || 1050}>
+				<ModalSpacingContextProvider value={variant === '24px' ? 6 : 5}>
+					<Box
+						ref={modal => {
+							this._modal = modal;
+							if (modal && modalWidth === null) {
+								this.setState({ modalWidth: modal.clientWidth });
+							}
+						}}
+						display="flex"
+						flexDirection="column"
+						justifyContent={fullscreen ? 'flex-start' : 'center'}
+						alignItems="center"
+						width={fullscreen ? ['100vw', '90vw'] : ['100vw', 'fit-content']}
+						height={fullscreen ? ['100vh', '90vh'] : 'fit-content'}
+						maxWidth={['100%', 'calc(100% - 32px)']}
+						maxHeight={fullscreen ? '100%' : ['100%', 'calc(100% - 32px)']}
+						margin="auto"
+						borderRadius={1}
+						backgroundColor={(theme && theme.background) || 'white'}
+						{...props}
+					>
+						<ModalHeader
+							title={title}
+							subtitle={subtitle}
+							onClose={onClose}
+							headerBottomBorder={headerBottomBorder}
+							variant={headerVariant}
+						/>
+						{doesChildrenIncludeModalContent ? children : <ModalContent>{children}</ModalContent>}
+						{!withoutFooter &&
+							(renderFooter ? (
+								renderFooter()
+							) : (
+								<DefaultModalFooter useFullWidthButtons={verticalButtons} {...footerProps} />
+							))}
+					</Box>
+				</ModalSpacingContextProvider>
+			</ModalBackdrop>
+		);
+	}
+
+	render() {
+		if (!this.props.isOpen || !this.state.canUseDom) {
+			return null;
+		}
+
+		if (this.targetContainer) {
+			return createPortal(this.renderModal(), this.targetContainer);
+		}
+
+		return this.renderModal();
+	}
+}
