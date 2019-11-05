58103418fa3b6eb632a58e49bc52e80ee795fcf3
Bryan Smith <bryanrsmith@gmail.com>
2019-11-05 11:11:54 -0800

    Remove accidentally-committed code.

--- theme/index.js
+++ theme/index.js
@@ -2,13 +2,10 @@ import { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import { theme as coreTheme } from './core';
 import { textStyles } from './textStyles';
-import { buttons, buttonSizes } from './buttons';
 
 export const theme = {
 	...coreTheme,
 	textStyles,
-	buttons,
-	buttonSizes,
 };
 
 export function useTheme() {
