d5750c3b08368e46f88969d7d7639b0faf77db28
Robert Bolender <robert.bolender@faithlife.com>
2019-11-05 17:05:23 -0800

    Refactor v6 Modal to a functional component.

--- components/modal/modal.jsx
+++ components/modal/modal.jsx
@@ -1,119 +1,99 @@
-import React from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import { Box } from '../Box';
-import { debouncedResize } from '../utils';
+import { useElementSize } from '../shared-hooks/useElementSize';
 import { ModalBackdrop } from '../modal-backdrop';
 import { ModalContextProvider } from './use-modal-context';
 
 /** A flexible component built on styled-system primitives. */
-export class Modal extends React.Component {
-	static propTypes = {
-		/** Controls state of modal. */
-		isOpen: PropTypes.bool.isRequired,
-		/** Callback function for when the modal is closed.  */
-		onClose: PropTypes.func.isRequired,
-		/** Consider using the Modal.Header, Modal.Content, and Modal.Footer components as children. */
-		children: PropTypes.node.isRequired,
-		/** Set to 'body' to attach the modal to body, otherwise will attach as a child element. */
-		container: PropTypes.string,
-		/** Will apply padding to Modal.Header, Modal.Content, and Modal.Footer child components. Uses the spacing scale from the theme. */
-		contentPadding: PropTypes.number,
-		/** Intended for modals with lots of functionality, such as media galleries or editors. */
-		fullscreen: PropTypes.bool,
-	};
+export const Modal = ({
+	isOpen,
+	container,
+	onClose,
+	children,
+	theme,
+	styleOverrides,
+	contentPadding,
+	fullscreen,
+	...props
+}) => {
+	const [size, containerRef] = useElementSize();
+	const [canUseDom, setCanUseDom] = useState(false);
+	const targetContainer = useRef(null);
 
-	static defaultProps = {
-		contentPadding: 5,
-	};
-
-	state = {
-		modalWidth: null,
-		canUseDom: false,
-	};
-
-	componentDidMount() {
-		const { cancel } = debouncedResize(this.handleResize);
-		this.cancelResizeListener = cancel;
-		this.setState({ canUseDom: true }); // eslint-disable-line
-
-		const { container } = this.props;
+	useEffect(() => {
 		if (container) {
 			if (typeof container === 'string') {
 				// must be an id or body
-				this.targetContainer =
+				targetContainer.current =
 					container === 'body' ? document.body : document.getElementById(container);
 			} else {
 				// must be a ref
-				this.targetContainer = typeof container === 'object' ? container.current : container();
+				targetContainer.current = typeof container === 'object' ? container.current : container();
 			}
 		}
-	}
+		setCanUseDom(true);
+	}, [container]);
 
-	componentWillUnmount() {
-		if (this.cancelResizeListener) {
-			this.cancelResizeListener();
-		}
-	}
-
-	handleResize = () => {
-		if (this._modal) {
-			this.setState({ modalWidth: this._modal.clientWidth });
-		}
-	};
-
-	renderModal() {
-		const {
-			onClose,
-			children,
-			theme,
-			styleOverrides,
+	const modalContext = useMemo(
+		() => ({
 			contentPadding,
-			fullscreen,
-			...props
-		} = this.props;
+			modalWidth: size ? size.width : null,
+		}),
+		[contentPadding, size],
+	);
 
-		const { modalWidth } = this.state;
-
-		return (
-			<ModalBackdrop onClose={onClose} zIndex={(styleOverrides && styleOverrides.zIndex) || 1050}>
-				<ModalContextProvider value={contentPadding}>
-					<Box
-						ref={modal => {
-							this._modal = modal;
-							if (modal && modalWidth === null) {
-								this.setState({ modalWidth: modal.clientWidth });
-							}
-						}}
-						display="flex"
-						flexDirection="column"
-						justifyContent={fullscreen ? 'flex-start' : 'center'}
-						alignItems="center"
-						width={fullscreen ? ['100vw', '90vw'] : ['100vw', 'fit-content']}
-						height={fullscreen ? ['100vh', '90vh'] : 'fit-content'}
-						maxWidth={['100%', 'calc(100% - 32px)']}
-						maxHeight={fullscreen ? '100%' : ['100%', 'calc(100% - 32px)']}
-						margin="auto"
-						borderRadius={1}
-						backgroundColor={(theme && theme.background) || 'white'}
-						{...props}
-					>
-						{children}
-					</Box>
-				</ModalContextProvider>
-			</ModalBackdrop>
-		);
+	if (!isOpen || !canUseDom) {
+		return null;
 	}
 
-	render() {
-		if (!this.props.isOpen || !this.state.canUseDom) {
-			return null;
-		}
+	const modal = (
+		<ModalBackdrop onClose={onClose} zIndex={(styleOverrides && styleOverrides.zIndex) || 1050}>
+			<ModalContextProvider value={modalContext}>
+				<Box
+					ref={containerRef}
+					display="flex"
+					flexDirection="column"
+					justifyContent={fullscreen ? 'flex-start' : 'center'}
+					alignItems="center"
+					width={fullscreen ? ['100vw', '90vw'] : ['100vw', 'fit-content']}
+					height={fullscreen ? ['100vh', '90vh'] : 'fit-content'}
+					maxWidth={['100%', 'calc(100% - 32px)']}
+					maxHeight={fullscreen ? '100%' : ['100%', 'calc(100% - 32px)']}
+					margin="auto"
+					borderRadius={1}
+					backgroundColor={(theme && theme.background) || 'white'}
+					{...props}
+				>
+					{children}
+				</Box>
+			</ModalContextProvider>
+		</ModalBackdrop>
+	);
 
-		if (this.targetContainer) {
-			return createPortal(this.renderModal(), this.targetContainer);
-		}
-
-		return this.renderModal();
+	if (targetContainer && targetContainer.current) {
+		return createPortal(modal, targetContainer.current);
 	}
-}
+
+	return modal;
+};
+
+Modal.propTypes = {
+	/** Controls state of modal. */
+	isOpen: PropTypes.bool.isRequired,
+	/** Callback function for when the modal is closed.  */
+	onClose: PropTypes.func.isRequired,
+	/** Consider using the Modal.Header, Modal.Content, and Modal.Footer components as children. */
+	children: PropTypes.node.isRequired,
+	/** Set to 'body' to attach the modal to body, otherwise will attach as a child element. */
+	container: PropTypes.string,
+	/** Will apply padding to Modal.Header, Modal.Content, and Modal.Footer child components. Uses the spacing scale from the theme. */
+	contentPadding: PropTypes.number,
+	/** Intended for modals with lots of functionality, such as media galleries or editors. */
+	fullscreen: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+	contentPadding: 5,
+};
