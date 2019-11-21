de5a9a19d7a88700e122abc8b4e6d7866d10fd84
Auresa Nyctea <auresa.nyctea@faithlife.com>
2019-11-20 20:17:01 -0800

    Add dummy data to group-selector example

--- examples/group-selector/index.jsx
+++ examples/group-selector/index.jsx
@@ -1,35 +1,149 @@
 import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 import { GroupSelector } from '@faithlife/group-selector';
+import { Accordion } from '@faithlife/styled-ui';
 
 export class App extends PureComponent {
 	state = {
-		groups: [],
+		groups: [
+			{
+				name: 'Michaels Test Church',
+				groupId: 1,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'participant',
+				membershipKind: 'admin',
+				claimable: false,
+			},
+			{
+				name: 'Example Community Church',
+				groupId: 2,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'participant',
+				membershipKind: 'admin',
+				claimable: false,
+			},
+			{
+				name: 'Jordans Test Church',
+				groupId: 3,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'participant',
+				membershipKind: 'admin',
+				claimable: false,
+			},
+			{
+				name: 'Cool Test Church',
+				groupId: 4,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'participant',
+				membershipKind: 'admin',
+				claimable: false,
+			},
+		],
 		selectedGroupId: 1,
 		mobile: false,
+		demoSearchResults: [
+			{
+				name: 'Wrong Kind',
+				groupId: 1,
+				kind: 'general',
+				avatarUrl: '',
+				relationshipKind: 'participant',
+				membershipKind: 'admin',
+				claimable: false,
+			},
+			{
+				name: 'Claimable Church',
+				groupId: 2,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'none',
+				claimable: true,
+			},
+			{
+				name: 'Not a Member',
+				groupId: 3,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'none',
+				claimable: false,
+			},
+			{
+				name: 'Member but Not Admin',
+				groupId: 4,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'participant',
+				membershipKind: 'member',
+				claimable: false,
+			},
+			{
+				name: 'Search result 5',
+				groupId: 5,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'participant',
+				membershipKind: 'admin',
+				claimable: false,
+			},
+			{
+				name: 'Search result 6',
+				groupId: 6,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'participant',
+				membershipKind: 'admin',
+				claimable: false,
+			},
+			{
+				name: 'Search result 7',
+				groupId: 7,
+				kind: 'church',
+				avatarUrl: '',
+				relationshipKind: 'participant',
+				membershipKind: 'admin',
+				claimable: false,
+			},
+		],
 	};
 	render() {
 		return (
 			<header>
 				<GroupSelector
-					onSearchInputChange={() => {
-						alert('handled by application');
+					onSearchInputChange={search => {
+						this.setState({ groupSearchResults: this.state.demoSearchResults });
 					}}
-					onCreateGroup={() => {
-						alert('handled by application');
+					onCreateGroup={(name, location) => {
+						const newGroup = {
+							name,
+							groupId: 5,
+							kind: 'church',
+							avatarUrl: '',
+							relationshipKind: 'participant',
+							membershipKind: 'admin',
+							claimable: false,
+						};
+
+						this.setState({
+							groups: [...this.state.groups, newGroup],
+							selectedGroupId: newGroup.groupId,
+						});
 					}}
-					onSelectionChange={() => {
-						alert('handled by application');
+					onSelectionChange={(groupId, name) => {
+						this.setState({ selectedGroupId: groupId });
 					}}
 					groups={this.state.groups}
 					selectedGroupId={this.state.selectedGroupId}
-					groupSearchResults={this.state.groups}
-					groupSelectorView={'no-groups'}
+					groupSearchResults={this.state.groupSearchResults || this.state.groups}
+					groupSelectorView={'groups'}
 					onGetStartedClick={() => {
-						alert('handled by application');
+						alert('Lets get started.');
 					}}
 					onClaimGroupClick={() => {
-						alert('handled by application');
+						alert('Claim the group!');
 					}}
 					onJoinGroupClick={() => {
 						alert('Should Join the group, or request');
