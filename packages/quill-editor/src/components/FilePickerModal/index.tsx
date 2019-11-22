bb6457e40635ff1e623cb9235015e1a329d8b39b
Daniel Stephenson <daniel.stephenson@faithlife.com>
2019-11-21 16:12:11 -0800

    fix: Update Modal to v6.

--- packages/quill-editor/src/components/FilePickerModal/index.tsx
+++ packages/quill-editor/src/components/FilePickerModal/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import {
-	Modal,
-	ModalContent,
 	FilePicker,
 	AmberContent,
 	TabManager,
@@ -11,6 +9,7 @@ import {
 	TabPanels,
 	TabPanel,
 } from '@faithlife/styled-ui';
+import { Modal } from '@faithlife/styled-ui/v6';
 import { useIsInternal } from '../../utility/useIsInternal';
 import { useLocalization } from '../Localization';
 import { FilePickerKind } from '../../QuillEditor';
@@ -22,14 +21,6 @@ interface IFilePickerModalProps {
 	groupId?: string;
 	pickerKind: FilePickerKind;
 }
-
-const Skeleton = styled.div`
-	width: 80vw;
-
-	& [role='tabpanel'] {
-		padding: 0;
-	}
-`;
 
 const TabSpace = styled.span`
 	width: 8px;
@@ -51,83 +42,84 @@ export const FilePickerModal: React.FunctionComponent<IFilePickerModalProps> = (
 	const res = useLocalization().image;
 	const isInternal = useIsInternal();
 	return (
-		<Modal
-			isOpen={showFilePicker}
-			onClose={closeFilePicker}
-			title={pickerKind === FilePickerKind.Image ? res.imageTitle : res.textTitle}
-			subtitle={pickerKind === FilePickerKind.Image ? res.subtitle : null}
-			headerBottomBorder={'none'}
-			withoutFooter
-		>
-			<ModalContent padding={0}>
-				<Skeleton>
-					{pickerKind === FilePickerKind.Image ? (
-						<FilePicker onFilesSelected={insertFile} onCancel={closeFilePicker} allowMultiSelect>
-							<TabManager>
-								<TabList>
-									{groupId && <Tab>{res.groupImages}</Tab>}
-									<Tab>{res.stockImages}</Tab>
-									<Tab>{res.unsplash}</Tab>
-									<TabSpace />
-								</TabList>
-								<TabPanels>
-									{groupId && (
-										<TabPanel>
-											<AmberContent
-												accountId={parseInt(groupId)}
-												filter={'kind:"image"'}
-												viewStyle={'tinygrid'}
-											/>
-										</TabPanel>
-									)}
-									<TabPanel>
+		<Modal isOpen={showFilePicker} onClose={closeFilePicker} fullscreen={true} withoutFooter>
+			<Modal.Header
+				title={pickerKind === FilePickerKind.Image ? res.imageTitle : res.textTitle}
+				subtitle={pickerKind === FilePickerKind.Image ? res.subtitle : null}
+				paddingBottom={'16px'}
+			/>
+			<Modal.Content padding={0}>
+				{pickerKind === FilePickerKind.Image ? (
+					<FilePicker onFilesSelected={insertFile} onCancel={closeFilePicker} allowMultiSelect>
+						<TabManager>
+							<TabList>
+								{groupId && <Tab>{res.groupImages}</Tab>}
+								<Tab>{res.stockImages}</Tab>
+								<Tab>{res.unsplash}</Tab>
+								<TabSpace />
+							</TabList>
+							<TabPanels display="grid">
+								{groupId && (
+									<TabPanel display="grid">
 										<AmberContent
-											accountId={stockImagesAmberAccount}
+											accountId={parseInt(groupId)}
 											filter={'kind:"image"'}
 											viewStyle={'tinygrid'}
+											height="100%"
 										/>
 									</TabPanel>
-									<TabPanel>
+								)}
+								<TabPanel display="grid">
+									<AmberContent
+										accountId={stockImagesAmberAccount}
+										filter={'kind:"image"'}
+										viewStyle={'tinygrid'}
+										height="100%"
+									/>
+								</TabPanel>
+								<TabPanel display="grid">
+									<AmberContent
+										accountId={isInternal ? unsplashInternalAmberAccount : unsplashAmberAccount}
+										viewStyle={'tinygrid'}
+										height="100%"
+									/>
+								</TabPanel>
+							</TabPanels>
+						</TabManager>
+					</FilePicker>
+				) : (
+					<FilePicker onFilesSelected={insertFile} onCancel={closeFilePicker} allowMultiSelect>
+						<TabManager>
+							<TabList>
+								<Tab>{res.textSnippets}</Tab>
+								{groupId && <Tab>{res.groupTextFiles}</Tab>}
+								<TabSpace />
+							</TabList>
+							<TabPanels display="grid">
+								<TabPanel display="grid">
+									<AmberContent
+										accountId={textSnippetsAmberAccount}
+										viewStyle={'tinygrid'}
+										footerText={' '}
+										height="100%"
+									/>
+								</TabPanel>
+								{groupId && (
+									<TabPanel display="grid">
 										<AmberContent
-											accountId={isInternal ? unsplashInternalAmberAccount : unsplashAmberAccount}
-											viewStyle={'tinygrid'}
-										/>
-									</TabPanel>
-								</TabPanels>
-							</TabManager>
-						</FilePicker>
-					) : (
-						<FilePicker onFilesSelected={insertFile} onCancel={closeFilePicker} allowMultiSelect>
-							<TabManager>
-								<TabList>
-									<Tab>{res.textSnippets}</Tab>
-									{groupId && <Tab>{res.groupTextFiles}</Tab>}
-									<TabSpace />
-								</TabList>
-								<TabPanels>
-									<TabPanel>
-										<AmberContent
-											accountId={textSnippetsAmberAccount}
+											accountId={parseInt(groupId)}
+											filter={'!kind:"image"'}
 											viewStyle={'tinygrid'}
 											footerText={' '}
+											height="100%"
 										/>
 									</TabPanel>
-									{groupId && (
-										<TabPanel>
-											<AmberContent
-												accountId={parseInt(groupId)}
-												filter={'!kind:"image"'}
-												viewStyle={'tinygrid'}
-												footerText={' '}
-											/>
-										</TabPanel>
-									)}
-								</TabPanels>
-							</TabManager>
-						</FilePicker>
-					)}
-				</Skeleton>
-			</ModalContent>
+								)}
+							</TabPanels>
+						</TabManager>
+					</FilePicker>
+				)}
+			</Modal.Content>
 		</Modal>
 	);
 };
