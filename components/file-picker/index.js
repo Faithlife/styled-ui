3c6219a14c9584520885b556998de1d3c861562b
Robert Bolender <robert.bolender@faithlife.com>
2019-12-27 16:49:36 -0800

    Add deprecation notice to FilePicker.

--- components/file-picker/index.js
+++ components/file-picker/index.js
@@ -1,3 +1,9 @@
-export { FilePicker } from './component';
+import { deprecateComponent } from '../utils';
+import { FilePicker as FilePickerComponent } from './component';
 export { AmberContent } from './amber-content';
 export { FileUpload } from './file-upload';
+
+export const FilePicker = deprecateComponent(
+	FilePickerComponent,
+	'The FilePicker component is leaving Styled-UI and is now available from the @faithlife/file-picker package.\nPlease see the FaithlifeEquipment repo for more details: https://git.faithlife.dev/Logos/FaithlifeEquipment',
+);
