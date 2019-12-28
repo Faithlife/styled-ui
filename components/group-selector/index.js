7f4d2b19eade46d3bec07e9bedd8948427ca4810
Robert Bolender <robert.bolender@faithlife.com>
2019-12-27 16:49:49 -0800

    Add deprecation notice to GroupSelector.

--- components/group-selector/index.js
+++ components/group-selector/index.js
@@ -1,5 +1,18 @@
-export { GroupSelector } from './component';
-export { LargeGroupSelector } from './large/component';
+import { deprecateComponent } from '../utils';
+import { GroupSelector as GroupSelectorComponent } from './component';
+import { LargeGroupSelector as LargeGroupSelectorComponent } from './large/component';
 
-// Deprecated
-export { LargeGroupSelector as GroupSelectorModal } from './large/component';
+export const GroupSelector = deprecateComponent(
+	GroupSelectorComponent,
+	'The GroupSelector component is leaving Styled-UI and is now available from the @faithlife/group-selector package.\nPlease see the FaithlifeEquipment repo for more details: https://git.faithlife.dev/Logos/FaithlifeEquipment',
+);
+
+const LargeGroupSelector = deprecateComponent(
+	LargeGroupSelectorComponent,
+	'The GroupSelector component is leaving Styled-UI and is now available from the @faithlife/group-selector package.\nPlease see the FaithlifeEquipment repo for more details: https://git.faithlife.dev/Logos/FaithlifeEquipment',
+);
+
+export {
+	LargeGroupSelector,
+	LargeGroupSelector as GroupSelectorModal, // Legacy deprecated component name
+};
