3b7cbb622ca029ca52a37eabb4b827c4cbcba89d
Robert Bolender <robert.bolender@faithlife.com>
2019-12-27 16:49:59 -0800

    Add deprecation notice to ProductDrawer.

--- components/product-drawer/index.js
+++ components/product-drawer/index.js
@@ -1,1 +1,7 @@
-export { ProductDrawer } from './component';
+import { deprecateComponent } from '../utils';
+import { ProductDrawer as ProductDrawerComponent } from './component';
+
+export const ProductDrawer = deprecateComponent(
+	ProductDrawerComponent,
+	'The ProductDrawer component is leaving Styled-UI and is now available from the @faithlife/product-drawer package.\nPlease see the FaithlifeEquipment repo for more details: https://git.faithlife.dev/Logos/FaithlifeEquipment',
+);
