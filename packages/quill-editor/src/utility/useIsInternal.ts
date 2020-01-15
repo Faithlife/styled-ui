bd0a1401ee44b1a641236c6e38aba78faba43f48
Robert Bolender <robert.bolender@faithlife.com>
2020-01-14 16:43:27 -0800

    fix: Treat Beta PR environments as not internal.

--- packages/quill-editor/src/utility/useIsInternal.ts
+++ packages/quill-editor/src/utility/useIsInternal.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const isInternalRegex = /https?:\/\/(local\.|internal\.|test\.|pr\d+\.|\d+\.|internal\.|localhost[:/])/;
+const isInternalRegex = /https?:\/\/(local\.|internal\.|test\.|pr\d+\.(?!beta)|\d+\.|internal\.|localhost[:/])/;
 
 export const useIsInternal = () => {
 	const [isInternal, setIsInternal] = useState<boolean>(false);
