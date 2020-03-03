a9c1a78719a8d3c123bf0f20e34e306a1aa96ac2
KorbinAncell <Korbin.ancell@faithlife.com>
2020-03-03 09:59:29 -0800

    feat: Add storybook configuration.

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+	stories: ['../examples/**/*.story.(js|mdx)'],
+	addons: [
+		'@storybook/addon-docs',
+		'@storybook/addon-actions',
+		'@storybook/addon-knobs',
+		'@storybook/addon-links',
+	],
+};
