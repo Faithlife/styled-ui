d7762edc22f27f14bf626b0c66cb7dd0e586f19d
LawrenceRafferty <lawrence.rafferty@logos.com>
2020-05-13 11:52:42 -0700

    feat(file-picker): Work around Amber auto sign in problem BWA-41714. (#112)

--- packages/file-picker/src/Tabs.jsx
+++ packages/file-picker/src/Tabs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TabManager, Tab, TabList, TabPanels, TabPanel } from '@faithlife/styled-ui';
 
 export const Tabs = ({ children }) => (
@@ -12,10 +12,29 @@ export const Tabs = ({ children }) => (
 			{React.Children.map(children, tab =>
 				tab ? (
 					<TabPanel display="grid" padding={tab.props.padding || 0}>
-						{React.cloneElement(tab)}
+						{({ selected }) => (
+							<RenderChildrenIfWasEverSelected selected={selected}>
+								{React.cloneElement(tab)}
+							</RenderChildrenIfWasEverSelected>
+						)}
 					</TabPanel>
 				) : null
 			)}
 		</TabPanels>
 	</TabManager>
 );
+
+// Waiting to create the tab's UI until it is selected works around a problem where we create
+// multiple Amber iframes that all try to auto sign in at the same time, which causes one of them
+// to succeed and the rest to fail because on the backend all of the requests were given the same
+// single use token.
+const RenderChildrenIfWasEverSelected = ({ children, selected }) => {
+	const [wasEverSelected, setWasEverSelected] = useState(selected);
+	useEffect(() => {
+		if (selected) {
+			setWasEverSelected(true);
+		}
+	}, [selected]);
+
+	return wasEverSelected ? children : null;
+};
