25fea3cc71dee3c95552d3e437fb714132f356d0
Bryan Smith <bryanrsmith@gmail.com>
2020-05-28 14:45:33 -0700

    Fix ThemedBox scrollbar styling.

--- components/ThemedBox.js
+++ components/ThemedBox.js
@@ -2,31 +2,30 @@ import styled, { css } from 'styled-components';
 import { Box } from './Box';
 import { theme } from '../theme';
 
-export const ThemedBox = styled(Box)``;
+export const ThemedBox = styled(Box)`
+	${({ theme }) => css`
+		div::-webkit-scrollbar-thumb {
+			border-radius: 10px;
+			background-color: ${theme.scrollbars.thumbBackground};
+		}
+
+		div::-webkit-scrollbar,
+		div::-webkit-scrollbar-corner {
+			background: ${theme.scrollbars.background};
+		}
+
+		div::-webkit-scrollbar {
+			height: ${theme.scrollbars.width};
+			width: ${theme.scrollbars.width};
+		}
+
+		@supports (scrollbar-color: #000 #000) {
+			scrollbar-color: ${theme.scrollbars.thumbBackground} ${theme.scrollbars.background};
+		}
+	`}
+`;
 
 ThemedBox.defaultProps = {
 	theme,
 	color: 'foregroundPrimary',
-	css: scrollbarCss,
 };
-
-export const scrollbarCss = ({ theme }) => css`
-	div::-webkit-scrollbar-thumb {
-		border-radius: 100px;
-		background-color: ${theme.scrollbars.thumbBackground};
-	}
-
-	div::-webkit-scrollbar,
-	div::-webkit-scrollbar-corner {
-		background: ${theme.scrollbars.background};
-	}
-
-	div::-webkit-scrollbar {
-		height: ${theme.scrollbars.width};
-		width: ${theme.scrollbars.width};
-	}
-
-	@supports (scrollbar-color: #000 #000) {
-		scrollbar-color: ${theme.scrollbars.thumbBackground} ${theme.scrollbars.background};
-	}
-`;
