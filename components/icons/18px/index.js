28ceff6df080674f257a676bf510aed18636f572
JeremyEinfeld <jeremy.einfeld@faithlife.com>
2020-08-07 16:59:06 -0700

    Add more common icons

--- components/icons/18px/index.js
+++ components/icons/18px/index.js
@@ -1,5 +1,6 @@
 export { default as AddNewCircleFilled } from './AddNewCircleFilled.svg';
 export { default as AddNewCircleOutline } from './AddNewCircleOutline.svg';
+export { default as Calendar } from './Calendar.svg';
 export { default as Change } from './Change.svg';
 export { default as Checkmark } from './Checkmark.svg';
 export { default as Clear } from './Clear.svg';
@@ -12,8 +13,12 @@ export { default as DraggableGrabHandleMove } from './DraggableGrabHandleMove.sv
 export { default as Expand } from './Expand.svg';
 export { default as FavoriteEmpty } from './FavoriteEmpty.svg';
 export { default as FavoriteFilled } from './FavoriteFilled.svg';
+export { default as Filter } from './Filter.svg';
 export { default as FloatUndock } from './FloatUndock.svg';
+export { default as Gear } from './Gear.svg';
 export { default as KebabHorizontal } from './KebabHorizontal.svg';
 export { default as KebabVertical } from './KebabVertical.svg';
+export { default as Search } from './Search.svg';
 export { default as Sort } from './Sort.svg';
+export { default as Trash } from './Trash.svg';
 export { default as X } from './X.svg';
