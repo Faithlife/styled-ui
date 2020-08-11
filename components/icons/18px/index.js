b889393a882c9f90f65ed565a687f569d71d0fe0
Jeremy Einfeld <jeremy.einfeld@faithlife.com>
2020-08-11 13:29:07 -0700

    Add more common icons (#355)

--- components/icons/18px/index.js
+++ components/icons/18px/index.js
@@ -1,5 +1,10 @@
 export { default as AddNewCircleFilled } from './AddNewCircleFilled.svg';
 export { default as AddNewCircleOutline } from './AddNewCircleOutline.svg';
+export { default as Calendar } from './Calendar.svg';
+export { default as CaretDown } from './CaretDown.svg';
+export { default as CaretLeft } from './CaretLeft.svg';
+export { default as CaretRight } from './CaretRight.svg';
+export { default as CaretUp } from './CaretUp.svg';
 export { default as Change } from './Change.svg';
 export { default as Checkmark } from './Checkmark.svg';
 export { default as Clear } from './Clear.svg';
@@ -12,8 +17,12 @@ export { default as DraggableGrabHandleMove } from './DraggableGrabHandleMove.sv
 export { default as Expand } from './Expand.svg';
 export { default as FavoriteEmpty } from './FavoriteEmpty.svg';
 export { default as FavoriteFilled } from './FavoriteFilled.svg';
+export { default as Filter } from './Filter.svg';
 export { default as FloatUndock } from './FloatUndock.svg';
+export { default as Gear } from './Gear.svg';
 export { default as KebabHorizontal } from './KebabHorizontal.svg';
 export { default as KebabVertical } from './KebabVertical.svg';
+export { default as Search } from './Search.svg';
 export { default as Sort } from './Sort.svg';
+export { default as Trash } from './Trash.svg';
 export { default as X } from './X.svg';
