5e5d00e15f20362e5b97050a470b1af5adb178bc
AuresaNyctea <auresa.nyctea@gmail.com>
2020-08-28 16:36:45 -0700

    Use formatOptionLabel

--- catalog/input/demos/custom-options-select.jsx
+++ catalog/input/demos/custom-options-select.jsx
@@ -1,11 +1,7 @@
-import React, { useState, useRef, useCallback } from 'react';
-import { FixedSizeList as List } from 'react-window';
-import { Box } from '../../../components/Box';
+import React, { useState, useCallback } from 'react';
 import { Select } from '../../../components/text-input-v2';
 import * as Styled from './styled.jsx';
 
-const menuItemSize = 56;
-const menuListMaxHeight = 400;
 export function CustomOptionsSelectDemo() {
 	const [selection, setSelection] = useState('');
 
@@ -23,52 +19,25 @@ export function CustomOptionsSelectDemo() {
 				onChange={handleGroupChanged}
 				isSearchable={false}
 				options={[
-					{ value: "washington", group: "Washington", link:"", subtitle: "yo", avatar:"https://files.logoscdn.com/v1/files/42382171/content.svg?signature=6V_cO8zkG4kob-qZTjNlJkCuNTA" },
-					{ value: "california", group: "California", link:"", subtitle: "yo", avatar:"https://files.logoscdn.com/v1/files/42382171/content.svg?signature=6V_cO8zkG4kob-qZTjNlJkCuNTA" },
-					{ value: "Texas", group: "Texas", link:"", subtitle: "yo", avatar:"https://files.logoscdn.com/v1/files/42382171/content.svg?signature=6V_cO8zkG4kob-qZTjNlJkCuNTA" }
+					{ value: "washington", group: "Washington", subtitle: "yo", avatar:"https://files.logoscdn.com/v1/files/42382171/content.svg?signature=6V_cO8zkG4kob-qZTjNlJkCuNTA" },
+					{ value: "california", group: "California", subtitle: "yo", avatar:"https://files.logoscdn.com/v1/files/42382171/content.svg?signature=6V_cO8zkG4kob-qZTjNlJkCuNTA" },
+					{ value: "Texas", group: "Texas", subtitle: "yo", avatar:"https://files.logoscdn.com/v1/files/42382171/content.svg?signature=6V_cO8zkG4kob-qZTjNlJkCuNTA" }
 				]}
 				placeholder="Choose a state..."
-				components={{ MenuList: VirtualizedMenuList }}
+				formatOptionLabel={MenuItem}
 			/>
 		</div>
 	);
 }
 
-const MenuListContainer = React.forwardRef(function MenuListContainer(props, ref) {
-	return <Box ref={ref} {...props} />;
-});
-
-function MenuItem({ data, index, style }) {
+function MenuItem({ value, group, subtitle, avatar }) {
 	return (
-		<Styled.MenuItem style={style}>
-			<Styled.Avatar src={data[index].props.data.avatar} />
+		<Styled.MenuItem>
+			<Styled.Avatar src={avatar} />
 			<Styled.GroupInfo>
-				<Styled.GroupName>{data[index].props.data.group}</Styled.GroupName>
-				<Styled.GroupSubtitle>{data[index].props.data.subtitle}</Styled.GroupSubtitle>
+				<Styled.GroupName>{group}</Styled.GroupName>
+				<Styled.GroupSubtitle>{subtitle}</Styled.GroupSubtitle>
 			</Styled.GroupInfo>
 		</Styled.MenuItem>
-	);
-}
-
-// TODO support grouped items or variable sizes if this is used elsewhere
-function VirtualizedMenuList({ innerRef, children: _children }) {
-	const listRef = useRef(null);
-
-	const children = React.Children.toArray(_children);
-	const itemCount = children.length;
-
-	return (
-		<List
-			ref={listRef}
-			outerRef={innerRef}
-			height={Math.min(menuListMaxHeight, itemCount * menuItemSize)}
-			itemCount={itemCount}
-			itemSize={menuItemSize}
-			width={'100%'}
-			itemData={children}
-			innerElementType={MenuListContainer}
-		>
-			{MenuItem}
-		</List>
 	);
 }
