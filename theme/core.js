25783f616412524da2fe5395438c1e0ce375a07f
AuresaNyctea <auresa.nyctea@gmail.com>
2020-09-08 11:05:51 -0700

    Update Select border color to gray34 (#361)
    * Update Select border color to gray34
    
    * Update icon color to match design request.

--- theme/core.js
+++ theme/core.js
@@ -156,13 +156,13 @@ colors.tab = {
 
 colors.input = {
 	foreground: colors.gray66,
-	border: colors.gray22,
+	border: colors.gray34,
 	borderFocused: '#278ed4',
 	shadowFocused: '#d0e6f6',
 	placeholderForeground: colors.gray22,
 	background: 'white',
 	backgroundReadOnly: colors.gray8,
-	icon: colors.gray22,
+	icon: colors.gray52,
 	iconFocused: colors.gray34,
 };
 
