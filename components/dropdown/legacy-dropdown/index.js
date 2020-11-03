cac08727c43e22e5676093c1db4cc4f05e0c052e
KorbinAncell <Korbin.ancell@faithlife.com>
2020-11-02 22:49:36 -0800

    Deprecate the v5 components.

--- components/dropdown/legacy-dropdown/index.js
+++ components/dropdown/legacy-dropdown/index.js
@@ -1,7 +1,43 @@
-export { Dropdown } from './component';
-export { MenuItem, MenuSeparator, MenuCheckbox } from './dropdown-children';
-export { DropdownMenu } from './dropdown-menu';
-export { DropdownToggle } from './dropdown-toggle';
+import { deprecateComponent } from '../../utils';
+
+import { Dropdown as DropdownComponent } from './component';
+import { DropdownMenu as DropdownMenuComponent } from './dropdown-menu';
+import { DropdownToggle as DropdownToggleComponent } from './dropdown-toggle';
+import {
+	MenuItem as MenuItemComponent,
+	MenuSeparator as MenuSeparatorComponent,
+	MenuCheckbox as MenuCheckboxComponent,
+} from './dropdown-children';
+
+const v6ImportHelpText = `You can opt into this new API by importing { Dropdown } from @faithlife/styled-ui/v6
+Learn more at https://faithlife.github.io/styled-ui/#/dropdown/variations/v6`;
+
+const depWarning = (name, newName) =>
+	`${name} will be moving to ${newName} in Styled-UI version 6.\n${v6ImportHelpText}`;
+
+const Dropdown = deprecateComponent(
+	DropdownComponent,
+	`Prefer the Styled-UI version 6 Dropdown.\n${v6ImportHelpText}`,
+);
+const DropdownMenu = deprecateComponent(
+	DropdownMenuComponent,
+	depWarning('DropdownMenu', 'Dropdown.Menu'),
+);
+const DropdownToggle = deprecateComponent(
+	DropdownToggleComponent,
+	depWarning('DropdownToggle', 'Dropdown.Toggle'),
+);
+const MenuItem = deprecateComponent(MenuItemComponent, depWarning('MenuItem', 'Dropdown.Item'));
+const MenuSeparator = deprecateComponent(
+	MenuSeparatorComponent,
+	depWarning('MenuSeparator', 'Menu.Separator'),
+);
+const MenuCheckbox = deprecateComponent(
+	MenuCheckboxComponent,
+	depWarning('MenuCheckbox', 'Dropdown.CheckboxItem'),
+);
+
+export { Dropdown, DropdownMenu, DropdownToggle, MenuItem, MenuSeparator, MenuCheckbox };
 export { useDropdownContext } from './dropdown-utils';
 export { DropdownCore } from './dropdown-core';
 export { DropdownToggleCore } from './dropdown-toggle-core';
