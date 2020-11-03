ae3d8181575e0c77c26f514fef998df3deaafeda
KorbinAncell <Korbin.ancell@faithlife.com>
2020-11-02 21:24:16 -0800

    Add documentation.

--- catalog/index.js
+++ catalog/index.js
@@ -497,6 +497,11 @@ const pages = [
 						justify-content: space-between;
 					`,
 					Button,
+					FavoriteFilled,
+					avatarSrc:
+						'https://files.logoscdn.com/v1/files/45929047/content.svg?signature=LanlyvDyHzxkch6f8DNUV1J15Lw',
+					thumbnailSrc:
+						'https://files.logoscdn.com/v1/files/46191785/assets/11156922/content.png?signature=ogDB6y5KmpyPWtBWitethsnveRw',
 				},
 			},
 			{
