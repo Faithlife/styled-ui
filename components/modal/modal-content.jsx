ce36c75e45ca075109c3f62bbc16e4d4344a187b
KorbinAncell <Korbin.ancell@faithlife.com>
2020-11-10 12:06:43 -0800

    Use themedBox for scrolling.

--- components/modal/modal-content.jsx
+++ components/modal/modal-content.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '../Box';
+import { ThemedBox } from '../ThemedBox';
 import { useModalContext } from './use-modal-context';
 
 /** A flexible component built on styled-system primitives. */
 export const ModalContent = ({ paddingY, children, ...props }) => {
 	const { contentPadding } = useModalContext();
 	return (
-		<Box paddingY={paddingY ?? contentPadding} overflowY="auto">
+		<ThemedBox paddingY={paddingY ?? contentPadding} overflow="hidden">
 			<Box
 				maxWidth="100%"
 				width={['100vw', 'auto']}
@@ -21,7 +22,7 @@ export const ModalContent = ({ paddingY, children, ...props }) => {
 			>
 				{children}
 			</Box>
-		</Box>
+		</ThemedBox>
 	);
 };
 
