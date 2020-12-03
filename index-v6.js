8ec765ef65465a76187c9cfff9b2c820e129d385
Ty Mick <ty@tymick.me>
2020-12-02 19:33:03 -0600

    Restore legacy component and separate exports

--- index-v6.js
+++ index-v6.js
@@ -1,4 +1,5 @@
-export { Modal } from './components/modal';
 export { Button, SegmentedButtonGroup } from './components/button';
-export { usePopover, Popover } from './components/popover-v6';
+export { DatePickerInput } from './components/date-picker-input';
 export { Dropdown } from './components/dropdown';
+export { Modal } from './components/modal';
+export { usePopover, Popover } from './components/popover-v6';
