87b95a0cc346e8f4e44dce0fe2e58e3542e3a780
Ty Mick <ty@tymick.me>
2020-12-04 08:58:24 -0600

    Adapt SimpleToast for v6 with Styled System

---
+++ components/simple-toast/component.jsx
@@ -0,0 +1,106 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import systemPropTypes from '@styled-system/prop-types';
+import Transition from 'react-transition-group/Transition';
+import { LegacyButton as Button } from '../button';
+import { X as Close } from '../icons/18px';
+import * as Styled from './styled.jsx';
+import { theme } from '../../theme';
+
+const transitionTime = Styled.transitionTime; // milliseconds
+
+/**
+ * A mobile-first Toast. A toast indicates that an action is being taken that does not require the user's
+ * specific attention.
+ */
+export class SimpleToast extends PureComponent {
+	static propTypes = {
+		/** In milliseconds */
+		showTime: PropTypes.number,
+		...systemPropTypes.color,
+		...systemPropTypes.layout,
+		...systemPropTypes.position,
+	};
+
+	static defaultProps = {
+		showTime: 1000,
+		theme,
+		styleOverrides: {},
+	};
+
+	state = {
+		toastHasEntered: false,
+		transitionIn: false,
+		messages: [],
+	};
+
+	_showTimeout = null;
+
+	componentWillUnmount() {
+		clearTimeout(this._showTimeout);
+	}
+
+	/** Accepts an object with a required `message` and an optional `icon` property */
+	showMessage = newMessage => {
+		if (newMessage.message) {
+			this.setState(prevState => ({
+				transitionIn: !prevState.toastHasEntered,
+				messages: [...prevState.messages, newMessage],
+			}));
+		}
+	};
+
+	triggerExit = () => this.setState({ transitionIn: false });
+
+	handleEntered = () => {
+		const { messages } = this.state;
+		this.setState({ toastHasEntered: true, transitionIn: messages.length === 1 });
+		this._showTimeout = setTimeout(this.triggerExit, this.props.showTime);
+	};
+
+	handleExited = () => {
+		const { messages } = this.state;
+		const newMessages = [...messages];
+		newMessages.shift();
+
+		clearTimeout(this._showTimeout);
+		this.setState({
+			messages: newMessages,
+			toastHasEntered: false,
+			transitionIn: newMessages.length > 0,
+		});
+	};
+
+	render() {
+		const { messages, transitionIn } = this.state;
+		const hasMultipleMessages = messages.length > 1;
+
+		return (
+			<Transition
+				in={transitionIn}
+				timeout={{
+					enter: transitionTime,
+					exit: hasMultipleMessages ? transitionTime / 2 : transitionTime,
+				}}
+				onEntered={this.handleEntered}
+				onExited={this.handleExited}
+			>
+				{state => (
+					<Styled.ToastContainer
+						state={state}
+						{...this.props}
+						onAnimationEnd={this.handleAnimationEnd}
+					>
+						{messages.length > 0 && messages[0].icon}
+						{messages.length > 0 && (
+							<Styled.ToastContent>{messages[0].message}</Styled.ToastContent>
+						)}
+						<Styled.ToastClose>
+							<Button variant="minorTransparent" icon={<Close />} onClick={this.triggerExit} />
+						</Styled.ToastClose>
+					</Styled.ToastContainer>
+				)}
+			</Transition>
+		);
+	}
+}
---
+++ components/simple-toast/styled.jsx
@@ -0,0 +1,137 @@
+import styled, { keyframes, css } from 'styled-components';
+import { color, layout, position } from 'styled-system';
+import { themeGet } from '@styled-system/theme-get';
+import { TransitionStatuses } from '../utils';
+
+const fixedHeaderHeight = 47; // px
+const fixedHeightOffset = 8; // px
+const toastOffset = { desktop: '24px', mobile: `${fixedHeaderHeight + fixedHeightOffset}px` };
+const toastMinWidth = '285px';
+const toastHeight = { desktop: '20px', mobile: '18px' };
+
+const slideIn = ({ bottom }) => keyframes`
+	from {
+		bottom: 0;
+	}
+
+	to {
+		bottom: ${(Array.isArray(bottom) ? bottom?.[1] || bottom?.[2] : bottom) || toastOffset.desktop};
+	}
+`;
+
+export const transitionTime = 250; // milliseconds
+
+export const ToastContainer = styled.div`
+	/** Shared Styles */
+	display: grid;
+	grid-auto-flow: column;
+	grid-column-gap: ${themeGet('space.3')};
+
+	grid-template-columns: min-content;
+
+	position: fixed;
+	z-index: 1000;
+
+	font-size: ${themeGet('fontSizes.4')};
+	font-weight: ${themeGet('fontWeights.regular')};
+	line-height: ${themeGet('lineHeights.ui')};
+
+	background-color: ${themeGet('colors.white')};
+	border-radius: 3px;
+	box-shadow: 0 19px 38px 0 rgba(0, 0, 0, 0.12), 0 15px 12px 0 rgba(0, 0, 0, 0.12);
+	opacity: 0;
+
+	pointer-events: none;
+
+	> svg {
+		height: 1em;
+		width: 1em;
+	}
+
+	/** Mobile Styles */
+	padding: 10px 16px;
+	height: ${toastHeight.mobile};
+	top: ${toastOffset.mobile};
+	left: 50%;
+	transform: translateX(-50%);
+
+	justify-items: center;
+
+	${props => {
+		switch (props.state) {
+			case TransitionStatuses.ENTERING:
+				return css`
+					transition: opacity ${transitionTime}ms linear;
+					opacity: 1;
+					pointer-events: all;
+				`;
+			case TransitionStatuses.EXITING:
+				return css`
+					transition: opacity ${transitionTime}ms linear;
+					opacity: 0;
+					pointer-events: all;
+				`;
+			case TransitionStatuses.ENTERED:
+				return css`
+					opacity: 1;
+					pointer-events: all;
+				`;
+			default:
+				return '';
+		}
+	}};
+
+	/** Desktop */
+	@media (min-width: ${themeGet('breakpoints.tablet')}) {
+		padding: 16px;
+		${({ width }) => (!width || !width?.[1] ? `min-width: ${toastMinWidth};` : '')}
+		height: ${toastHeight.desktop};
+
+		right: ${toastOffset.desktop};
+
+		top: auto;
+		left: auto;
+		transform: none;
+		justify-items: left;
+
+		bottom: ${toastOffset.desktop};
+
+		${props => {
+			switch (props.state) {
+				case TransitionStatuses.ENTERING:
+					return css`
+						animation: ${slideIn(props)} ${transitionTime}ms linear;
+						pointer-events: all;
+					`;
+				case TransitionStatuses.EXITING:
+					return css`
+						transition: opacity ${transitionTime}ms linear;
+						opacity: 0;
+						pointer-events: all;
+					`;
+				case TransitionStatuses.ENTERED:
+					return css`
+						opacity: 1;
+						pointer-events: all;
+					`;
+				default:
+					return '';
+			}
+		}};
+	}
+
+	&& {
+		${color}
+		${layout}
+		${position}
+	}
+`;
+
+export const ToastContent = styled.div`
+	white-space: nowrap;
+`;
+
+export const ToastClose = styled.div`
+	cursor: pointer;
+	justify-self: right;
+`;
