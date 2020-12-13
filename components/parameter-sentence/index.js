d43430d542caab2bf3650c99278c6393d38a84b9
Ty Mick <ty@tymick.me>
2020-12-13 00:14:53 -0600

    Add ParameterInputBox v6 exports

--- components/parameter-sentence/index.js
+++ components/parameter-sentence/index.js
@@ -1,3 +1,4 @@
 export { ParameterSelect as LegacyParameterSelect } from './legacy-parameter-select';
 export { ParameterInputBox as LegacyParameterInputBox } from './legacy-parameter-input';
 export { ParameterSentence } from './parameter-sentence';
+export { ParameterInputBox } from './parameter-input';
--- index-v6.js
+++ index-v6.js
@@ -1,5 +1,5 @@
 export { Button, SegmentedButtonGroup } from './components/button';
 export { Dropdown } from './components/dropdown';
 export { Modal } from './components/modal';
-export { ParameterSentence } from './components/parameter-sentence';
+export { ParameterSentence, ParameterInputBox } from './components/parameter-sentence';
 export { usePopover, Popover } from './components/popover-v6';
