a55a5f165e9479eba8d9666b54ab7b86b941ff3c
Ty Mick <ty@tymick.me>
2020-12-14 12:03:34 -0600

    Create v6 Slider with Styled System (#404)
    * Move v5 Slider to "Legacy" export
    
    * Add v6 Slider component rebuilt with Styled System
    
    * Add v6 Slider documentation
    
    * Prefer nullish coalescing operator

--- index-v6.js
+++ index-v6.js
@@ -1,4 +1,5 @@
-export { Modal } from './components/modal';
 export { Button, SegmentedButtonGroup } from './components/button';
-export { usePopover, Popover } from './components/popover-v6';
 export { Dropdown } from './components/dropdown';
+export { Modal } from './components/modal';
+export { usePopover, Popover } from './components/popover-v6';
+export { Slider } from './components/slider';
