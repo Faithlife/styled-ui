c45900cc274fdd8a284cad674c9e63ba2edaa206
Ty Mick <ty@tymick.me>
2020-12-17 20:19:32 -0600

    Fix InferredText legacy export

--- components/legacy-text-input/index.js
+++ components/legacy-text-input/index.js
@@ -1,5 +1,5 @@
 // These exports are deprecated!
 
 export { Typeahead, AsyncTypeahead, Token, Menu, MenuItem } from './typeahead';
-export { InferredText } from '../text-input-v2';
+export { InferredText } from '../text-input';
 export { InferredTypeahead } from './inferred-typeahead';
