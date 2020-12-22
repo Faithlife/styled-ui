0a3954accf80072b209ef24021a5cceda3474064
Ty Mick <ty@tymick.me>
2020-12-21 18:31:45 -0600

    Set up Jest/Enzyme for snapshot tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+	moduleNameMapper: {
+		'\\.svg$': '<rootDir>/tests/__mocks__/svgrMock.js',
+	},
+	setupFiles: ['./tests/jest.setup.js'],
 	testTimeout: 60000,
 };
---
+++ tests/__mocks__/svgrMock.js
@@ -0,0 +1,7 @@
+// see https://react-svgr.com/docs/jest/
+
+/* eslint-disable no-unused-vars */
+import * as React from 'react';
+
+export default 'SvgrURL';
+export const ReactComponent = 'div';
