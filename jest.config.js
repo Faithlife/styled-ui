2cada6b2f021ae2a8cef8ff53ad5a514cf2c469c
KorbinAncell <Korbin.ancell@faithlife.com>
2021-05-12 17:29:43 -0700

    Set up jest and remove mocha stuff.

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	testEnvironment: 'jest-environment-jsdom',
+	moduleNameMapper: {
+		// '\\.(css|less)$': require.resolve('./__mocks__/styleMock.js'),
+		'\\.svg': require.resolve('./__mocks__/svgMock.js'),
+	},
+	testPathIgnorePatterns: ['/node_modules/'],
+	testMatch: ['<rootDir>/**/*.test.{ts,tsx,js,jsx}'],
+	setupFiles: ['core-js'],
+	setupFilesAfterEnv: [
+		'babel-polyfill',
+		'@testing-library/jest-dom/extend-expect',
+		'jest-axe/extend-expect',
+	],
+	moduleDirectories: ['node_modules', '*/node_modules'],
+};
---
+++ test-utils/index.js
@@ -0,0 +1,5 @@
+import userEvent from '@testing-library/user-event';
+
+export * from '@testing-library/react';
+export * from 'jest-axe';
+export { userEvent };
