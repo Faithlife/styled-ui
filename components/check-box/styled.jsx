d92f80078b7a931f266d3393039df964a15a30a3
KorbinAncell <Korbin.ancell@faithlife.com>
2021-07-13 16:15:32 -0700

    Fix css vars in checkboxes.

--- components/check-box/styled.jsx
+++ components/check-box/styled.jsx
@@ -100,8 +100,9 @@ export const CheckedIndicator = styled.div`
 
 	&:after {
 		background-image: url("data:image/svg+xml;charset=utf8,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%208%208'%3E%3Cpath%20fill='${props =>
-			encodeURIComponent(
-				props.theme.colors.checkbox.primary,
+			props.theme.colors.checkbox.primary.replace(
+				'#',
+				'%23',
 			)}'%20d='M6.564.75l-3.59%203.612-1.538-1.55L0%204.26%202.974%207.25%208%202.193z'/%3E%3C/svg%3E");
 		color: ${({ theme }) => theme.colors.checkbox.primary};
 		opacity: 0;
