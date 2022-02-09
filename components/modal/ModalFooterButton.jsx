76d3ab2c004f88624ae6712a8d2889b6192666cb
Ty Mick <ty.mick@faithlife.com>
2022-02-09 09:40:08 -0800

    refactor: Extract modal footer button defaults

---
+++ components/modal/ModalFooterButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from '../button';
+
+/** A `Button` with size and padding defaults that conform to our modal footer design specs. */
+export const ModalFooterButton = ({ floatAcross = false, ...props }) => (
+	<Button
+		size={['medium', 'small']}
+		paddingInline={[4, '10px']}
+		marginInlineEnd={floatAcross ? 'auto' : null}
+		{...props}
+	/>
+);
+
+ModalFooterButton.propTypes = {
+	...Button.propTypes,
+	/**
+	 * Floats this button (and all after it) to the opposite side of the modal footer (the left side, for left-to-right
+	 * languages).
+	 */
+	floatAcross: PropTypes.bool,
+};
