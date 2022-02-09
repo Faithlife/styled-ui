d3e6d03b30c336a7604737dbbfc65950b66ea2d1
Ty Mick <ty.mick@faithlife.com>
2022-02-09 11:36:26 -0800

    feat: Allow external `ModalFooterButton` use
    Deprecating the `commitButton`, `cancelButton`, and `deleteButton` props
    in the process.
    
    - [Styled UI Flowdock thread][1]
    
    [1]: https://www.flowdock.com/app/logos/styled-ui/threads/bFNRZ1UAThh0VnkNx9fpBbrrOPm
    
    Co-authored-by: PatrickNausha <patrick.nausha@gmail.com>

--- components/modal/index.js
+++ components/modal/index.js
@@ -3,10 +3,12 @@ import { ModalHeader } from './ModalHeader';
 import { ModalContent } from './ModalContent';
 import { ModalFooter } from './ModalFooter';
 import { ModalFooterButtons } from './ModalFooterButtons';
+import { ModalFooterButton } from './ModalFooterButton';
 
 Modal.Header = ModalHeader;
 Modal.Content = ModalContent;
 Modal.Footer = ModalFooter;
 Modal.FooterButtons = ModalFooterButtons;
+Modal.FooterButton = ModalFooterButton;
 
 export { Modal };
